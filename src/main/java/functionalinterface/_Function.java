507500486f371e8cf5a488cd57d9e45f65739bd0
implermine <lkorea12345@gmail.com>
2022-02-22 21:13:02 +0900

    Predicate까지 수행하였고 Supplier부터 시작하면 됨
    custom functional interface 필요

---
+++ src/main/java/functionalinterface/_Function.java
@@ -0,0 +1,57 @@
+package functionalinterface;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class _Function {
+
+    public static void main(String[] args) {
+
+        // Function takes 1 argument and produces 1 result
+        int increment = incrementByOne(1);
+        System.out.println(increment);
+
+        int increment2 = incrementByOneFunction.apply(1);
+        System.out.println(increment2);
+
+        int multiply = multiplyBy10Function.apply(increment2);
+        System.out.println(multiply);
+
+        Function<Integer, Integer> addBy10AndThenMultiplyBy10 =
+                incrementByOneFunction.andThen(multiplyBy10Function);
+        System.out.println(addBy10AndThenMultiplyBy10.apply(1));
+
+        // BiFunction takes 2 argument and produces 1 result
+        Integer biFunctionResult = incrementByOneAndMultiplyBiFunction.apply(4, 100);
+        System.out.println("biFunctionResult = " + biFunctionResult);
+        int normalFunctionResult = incrementByOneAndMultiply(4, 100);
+        System.out.println("normalFunctionResult = " + normalFunctionResult);
+    }
+
+    static Function<Integer, Integer> incrementByOneFunction = number -> {
+        return number += 1;
+    };
+
+    static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
+
+
+
+    // this is target function that is replaced by functional interface...?
+    static int incrementByOne(int number) {
+        return number + 1;
+    }
+
+    //==================================================================
+
+    //these two are exactly same
+    // 1
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBiFunction =
+            (numberToIncrementByOne, numberToMultiplyBy)
+                    -> (numberToIncrementByOne + 1) * numberToMultiplyBy;
+
+    // 2
+    static int incrementByOneAndMultiply(int number, int numToMultiplyBy) {
+        return (number + 1) * numToMultiplyBy;
+    }
+    //====================================================================
+}
